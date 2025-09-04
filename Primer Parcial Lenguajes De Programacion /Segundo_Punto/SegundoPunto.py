f0478bb5df923b6be3a2a0673fac8cfced918f25
Samuel Leyton Muñoz <145390285+samuelleyton2006@users.noreply.github.com>
2025-09-03 22:48:09 -0500

    Update SegundoPunto.py

--- Primer Parcial Lenguajes De Programacion /Segundo_Punto/SegundoPunto.py
+++ Primer Parcial Lenguajes De Programacion /Segundo_Punto/SegundoPunto.py
@@ -1,13 +1,12 @@
-def afd_id(input_str):
+def AutomataID(input_str):
     if not input_str:
         return False
-    if not ((input_str >= 'a' and input_str <= 'z') or (input_str >= 'A' and input_str <= 'Z')):
+    first = input_str[0]
+    if not ((first >= 'a' and first <= 'z') or (first >= 'A' and first <= 'Z')):
         return False
     for c in input_str[1:]:
         if not ((c >= 'a' and c <= 'z') or (c >= 'A' and c <= 'Z') or (c >= '0' and c <= '9')):
             return False
     return True
-
-# Pruebas
 pruebas = ['a123', 'Zalgo', 'juan99', '9inicio', 'a-b']
-print([(s, afd_id(s)) for s in pruebas])
+print([(s, AutomataID(s)) for s in pruebas])
